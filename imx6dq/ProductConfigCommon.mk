861ebed89c164c17e552297e6086e70d21aab83d
Filipe Alves <developer@bithium.com>
2020-01-29 10:49:20 +0000

    CHG: Expand product `generic` inheritance.

--- imx6dq/ProductConfigCommon.mk
+++ imx6dq/ProductConfigCommon.mk
@@ -1,5 +1,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/generic.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_no_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
+
 ifeq ($(PRODUCT_IMX_CAR),true)
 $(call inherit-product, packages/services/Car/car_product/build/car.mk)
 endif
